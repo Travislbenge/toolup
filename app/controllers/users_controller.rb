2b10be22f14537a8a2900f79efa330417503f1ac
Travis Benge <travis.l.benge@gmail.com>
2018-05-21 18:45:27 +0200

    completed controllers, round 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to user_session_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    @tool = User.find(params[:id])
+    @user.destroy
+    redirect_to destroy_user_session_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :encrypted_password, :user_address)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  get 'tools/index'
-  get 'tools/show'
-  get 'tools/new'
-  get 'tools/create'
-  get 'tools/edit'
-  get 'tools/update'
-  get 'tools/destroy'
-  get 'users/new'
-  get 'users/create'
-  get 'users/destroy'
+  resources :users, only: [:new, :create, :destroy] do
+    resources :tools
+  end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
