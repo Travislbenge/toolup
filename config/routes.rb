961c91b2aef94d6cf646f55ad9445f24a0f15aac
Travis Benge <travis.l.benge@gmail.com>
2018-05-21 18:15:40 +0200

    Fixed models and started controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'tools/index'
+  get 'tools/show'
+  get 'tools/new'
+  get 'tools/create'
+  get 'tools/edit'
+  get 'tools/update'
+  get 'tools/destroy'
+  get 'users/new'
+  get 'users/create'
+  get 'users/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
