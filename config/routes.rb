d20e1fbbf2ca8b16595f34564085cc6b72bab5f4
Yatan Blu <yatanblu@gmail.com>
2018-05-23 15:38:13 +0200

    booking magic, nested bookings in routes, still left bookingnew superfluos

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
 
   root to: 'pages#homepage'
   get 'my_tools', to: 'tools#mine'
-  resources :tools
+  resources :tools do
+    resources :bookings, only: [:new, :create]
+  end
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :bookings
-
+  resources :bookings, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
