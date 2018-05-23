19d5acdbe383d220abae809947a6e3d5ca206600
Yatan Blu <yatanblu@gmail.com>
2018-05-23 11:48:02 +0200

    Booking Controller, Booking Policy, Added booking routes, seeded one booking, added booking policy, fixed tools controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,59 @@
+class BookingsController < ApplicationController
+  # This is the bookingscontroller, superfluous commenting
+  def index
+    @bookings = policy_scope(Booking)
+    @bookings = Booking.all
+    # @user = User.find(params[:id])
+    # @booking.user = @user
+    authorize @bookings
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
+
+  def new
+    @booking = Booking.new
+    authorize @booking
+  end
+
+  def create
+    @booking = Booking.create(booking_params)
+    authorize @booking
+    @user = User.find(params[:id])
+    # Linking the models
+    @booking.user = @user
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    @booking.update(booking_params)
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    @booking.destroy(booking_params)
+    redirect_to bookings_path
+  end
+
+private
+
+  def booking_params
+      params.require(:booking).permit(:tool_id, :user_id)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   root to: 'pages#homepage'
 
   resources :tools
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  resources :bookings
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
