9406885f4beb1a2e11b69e40955decd61103bb04
Travis Benge <travis.l.benge@gmail.com>
2018-05-25 15:56:18 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,8 @@ puts "building ten tools"
     user_id: u.id
     )
   tool.remote_photo_url = url
-  user.facebook_picture_url = "https://scontent-amt2-1.xx.fbcdn.net/v/t1.0-9/1930468_29324892422_1599_n.jpg?_nc_cat=0&oh=f9a96e3e9abb4f8b85c521ada9cb6ffe&oe=5B90284B"
+  u.facebook_picture_url = "https://scontent-amt2-1.xx.fbcdn.net/v/t1.0-9/1930468_29324892422_1599_n.jpg?_nc_cat=0&oh=f9a96e3e9abb4f8b85c521ada9cb6ffe&oe=5B90284B"
+  u_2.facebook_picture_url = "https://scontent-amt2-1.xx.fbcdn.net/v/t1.0-9/1930468_29324892422_1599_n.jpg?_nc_cat=0&oh=f9a96e3e9abb4f8b85c521ada9cb6ffe&oe=5B90284B"
   tool.save
   puts "waiting..."
   Booking.create(tool_id: tool.id, user_id: u.id)
